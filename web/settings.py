66fad63d9e90a0a4aba1a13dd2bb73fee0da61ea
Ayumu-Nono <ayumu.bete@gmail.com>
2020-06-27 17:39:30 +0900

    hide SECRET

--- web/settings.py
+++ web/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#q2so7qxhc*a1e@f4g8qhl^p5boij#eowhclj$8)p_oijl!(3i'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
