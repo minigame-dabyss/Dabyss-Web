c707f1e6611546500cad78cc2a9a1919675b857b
kataeeee <tomoya.katae@gmail.com>
2020-09-05 12:23:10 +0900

    ヘッダーとフッター途中

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,38 +9,34 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  logo: {
-    maxWidth: 60,
-    marginRight: theme.spacing(1),
-  },
 }));
 
 const Header = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div>
       <MuiThemeProvider theme={theme}>
         <AppBar className={classes.appBar} position="static">
           <Toolbar>
             <Grid container>
               <Grid item xs={6} className={classes.left}>
-                <a href="#">
-                  <img src="./logo_Dabyss.png" className={classes.logo} />
-                </a>
-                <Typography variant="h6" className={classes.title}>
-                  <a href="#">MinigameDabyss</a>
-                </Typography>
-              </Grid>
-              <Grid item xs={6} className={classes.right}>
-                <Button color="inherit" className={classes.games}>
-                  ゲーム一覧
-                </Button>
+                <Button color="inherit">ゲーム一覧</Button>
                 <Button color="inherit">ブログ</Button>
                 <Button color="inherit">お問い合わせ</Button>
               </Grid>
+              <Grid item xs={6} className={classes.right}>
+                <Button color="inherit" className="fa fa-plus-circle">
+                  LINE
+                </Button>
+                <Button color="inherit">Twitter</Button>
+                <Button color="inherit">Instagram</Button>
+              </Grid>
             </Grid>
           </Toolbar>
+          <Typography variant="subtitle2" classes={classes.copyright}>
+            © 2020 Copyright: Dabyss
+          </Typography>
         </AppBar>
       </MuiThemeProvider>
     </div>
