2385280e5fe600450d3ab07aa6cf903305511b70
kataeeee <tomoya.katae@gmail.com>
2020-09-13 10:41:15 +0900

    migrationゴニョゴニョ

--- articles/models.py
+++ articles/models.py
@@ -14,7 +14,7 @@ class HomeArticle(models.Model):
 
 class BlogArticle(models.Model):
     title = models.CharField(max_length=40)
-    # author = models.TextField('著者', blank=False)
+    author = models.TextField('著者', blank=False)
     summary = models.TextField('概要', blank=False)
     text = MarkdownxField('本文', help_text='Markdown形式で書いてください。')
     date = models.DateTimeField(blank=False)
