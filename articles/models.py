d0699ed99f2ad1363a4798a00434718d58ad7c6e
kataeeee <tomoya.katae@gmail.com>
2020-09-22 17:05:49 +0900

    slugの消去、日付だけ表示に変更

--- articles/models.py
+++ articles/models.py
@@ -14,11 +14,11 @@ class HomeArticle(models.Model):
 
 class BlogArticle(models.Model):
     title = models.CharField(max_length=40)
-    slug = models.CharField('URL', blank=False, max_length=20)
+    # slug = models.CharField('URL', blank=False, max_length=20)
     author = models.CharField('著者', blank=False, max_length=40)
     summary = models.TextField('概要', blank=False)
     text = MarkdownxField('本文', help_text='Markdown形式で書いてください。')
-    date = models.DateTimeField(blank=False)
+    date = models.DateField(blank=False)
     sumnail = models.ImageField(
         upload_to='blog_sumnail/',
         default="logo_Dabyss.png")
