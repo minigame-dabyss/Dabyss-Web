281009aca0a81d8b17d71c107ac342ea503c145c
kataeeee <tomoya.katae@gmail.com>
2020-10-30 11:53:03 +0900

    ポスグレを作った

--- web/settings.py
+++ web/settings.py
@@ -108,8 +108,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dabyss-web',
+        'USER': 'dabyss',
+        'PASSWORD': 'dabyss',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
