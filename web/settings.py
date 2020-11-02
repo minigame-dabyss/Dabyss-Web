698f4cf7237b40aff8924b11b49d829c19bb1d9d
kanichiki <kansei.ichiki@gmail.com>
2020-11-02 16:15:00 +0900

    adminのCSSが適用されないバグ修正

--- web/settings.py
+++ web/settings.py
@@ -159,7 +159,8 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
 
 
